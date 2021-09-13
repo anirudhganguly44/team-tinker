5817f29367d30470cf08c1dbcd8eee5f33566d0b
sheemambabu <46585696+sheemambabu@users.noreply.github.com>
2021-09-13 02:24:52 -0400

    FrontEnd and BackEnd changes to save images and display

--- SELFIE/writer/image_writer.py
+++ SELFIE/writer/image_writer.py
@@ -1,6 +1,7 @@
 import os, sys,shutil
 from matplotlib import pyplot
 import cv2
+import numpy as np
 
 # Code to write to the file
 
@@ -23,9 +24,29 @@ class image_writer():
                 print("Image not present!")
             else:
                 filename = path+str(sample.true_label)+".png"
-                pyplot.imshow(sample.image)
-                pyplot.imsave(filename,sample.image)
-                #cv2.imwrite(filename,sample.image)
+                # pyplot.imshow(sample.image)
+                # pyplot.imsave(filename,sample.image)
+                #img = cv2.cvtColor(sample.image, cv2.COLOR_GRAY2BGR)
+
+                #Sharpen the image
+                kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
+                img = cv2.convertScaleAbs(sample.image, alpha=(255.0))
+                im = cv2.filter2D(img, -1, kernel)
+                
+                # resize image
+                scale_percent = 500 # percent of original size
+                width = int(im.shape[1] * scale_percent / 100)
+                height = int(im.shape[0] * scale_percent / 100)
+                dim = (width, height)
+  
+                resized = cv2.resize(im, dim, interpolation = cv2.INTER_AREA)
+ 
+                #print('Resized Dimensions : ',resized.shape)
+ 
+                #re_img = cv2.imshow("Resized image", resized)
+                #cv2.waitKey(0)
+                #cv2.destroyAllWindows()
+                cv2.imwrite(filename,resized)
                 # try:
                 #     shutil.copy(sample.image, str(path+'.jpg'))
                 #     print("Copied file: "+str(sample.last_corrected_label)+" to folder: "+str(path))
