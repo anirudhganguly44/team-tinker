3cd8344952ce63531a8f70ef50a4b1064264ad42
sheemambabu <sheemambabu@outlook.com>
2021-11-23 23:09:40 -0600

    UI Changes

--- FrontEnd/client/src/components/CreateDataSet.jsx
+++ FrontEnd/client/src/components/CreateDataSet.jsx
@@ -1,90 +1,87 @@
 import React from "react";
 import { withRouter } from 'react-router-dom';
-import queryString from 'query-string';
-import Select from 'react-select';
 import axios from 'axios';
-import {Progress} from 'reactstrap';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import ReactLoading from "react-loading";
 
 class CreateDataSet extends React.Component {
   constructor() {
     super();
     this.state = {
-    data: [],
-    datasetname: "",
-    label: "",
-    count: 0,
-    label_count: 0,
-    filter_label: "default",
+      data: [],
+      datasetname: "",
+      label: "",
+      count: 0,
+      label_count: 0,
+      filter_label: "default",
+      setDone: false,
     };
   }
 
   componentDidMount() {
 
     var dataset = document.getElementById('dataset_name').value;
-    if(dataset != "")
-    {
-    fetch("/getimages?dir=./client/public/selfie-output/" + dataset)
-      .then((res) => res.json())
-      .then((json) => {
-        this.setState({ data: json.express });
+    if (dataset != "") {
+      fetch("/getimages?dir=./client/public/selfie-output/" + dataset)
+        .then((res) => res.json())
+        .then((json) => {
+          this.setState({ data: json.express });
 
-      });
+        });
     }
   }
 
-  Generate= (evt) =>{
+  Generate = (evt) => {
     var dataset = document.getElementById('dataset_name').value;
     var label = document.getElementById('label').value;
     var count = document.getElementById('count').value;
 
     var requestBody = {
-        "Dataset": dataset,
-        "Label":label,
-        "Count": count
-      };
+      "Dataset": dataset,
+      "Label": label,
+      "Count": count
+    };
 
-    axios.put("http://localhost:3001/create", requestBody).then(() =>
-    {
+    axios.put("http://localhost:3001/create", requestBody).then(() => {
       fetch("/getimages?dir=./client/public/selfie-output/" + this.state.datasetname)
-      .then((res) => res.json())
-      .then((json) => {
-        var label_count = this.getTrueLabels(json.express).length
-        this.setState({ data: json.express});
+        .then((res) => res.json())
+        .then((json) => {
+          toast.success('rename success');
+          var label_count = this.getTrueLabels(json.express).length
+          this.setState({ data: json.express });
+        })
+
+      this.forceUpdate()
     })
+  }
 
-    this.forceUpdate()
-  })
-}
+  Save = (evt) => {
+    window.open("/displaydatasets")
+  }
 
-Save=(evt) => {
-  window.open("/displaydatasets")
-}
+  Cancel = (evt) => {
 
-Cancel=(evt) => {
+    var requestBody = {
+      "Dataset": this.state.datasetname,
+    };
 
-  var requestBody = {
-    "Dataset": this.state.datasetname,
-  };
+    axios.delete("http://localhost:3001/deletedataset?dir=" + this.state.datasetname).then(() => {
+      window.open("/displaydatasets")
+    })
+  }
 
-  axios.delete("http://localhost:3001/deletedataset?dir=" + this.state.datasetname).then(() =>
-  {
-  window.open("/displaydatasets")
-  })
-}
+  canShowImage(file) {
 
-canShowImage(file) {
-  
     if (this.state.filter_label === "default")
       return true;
     if (file.label === this.state.filter_label)
       return true;
-  
 
-  return false;
 
-}
+    return false;
+
+  }
 
   getTrueLabels(dataset) {
     console.log("In finding true labels!");
@@ -115,7 +112,7 @@ canShowImage(file) {
 
     this.forceUpdate();
   }
-  
+
   handleDatasetChange = (e) => {
     this.setState({ datasetname: e.target.value });
   };
@@ -136,16 +133,17 @@ canShowImage(file) {
     return (
       <div>
         <div class="wrapper-create">
-          <label>Dataset Name</label> <br/>
-          <input type="text" size="100" value={this.state.datasetname} onChange={this.handleDatasetChange}  id="dataset_name" ></input> <br/> <br/>
-          <label>Label</label> <br/>
-          <input type="text" size="100" value={this.state.label} onChange={this.handleLabelChange}  id="label"></input> <br/> <br/> 
-          <label>Number of Images</label> <br/>
-          <input type="text" size="100" value={this.state.count} onChange={this.handleCountChange} id="count"></input> <br/> <br/> 
-          <input type="button" class="myButton_create"value="Generate" onClick={this.Generate} />
-          <input type="button" class="myButton_create"value="Save" onClick={this.Save} />
-          <input type="button" class="myButton_create"value="Cancel" onClick={this.Cancel} />
-          <br/>
+          <ToastContainer />
+          <label>Dataset Name</label> <br />
+          <input type="text" size="100" value={this.state.datasetname} onChange={this.handleDatasetChange} id="dataset_name" ></input> <br /> <br />
+          <label>Label</label> <br />
+          <input type="text" size="100" value={this.state.label} onChange={this.handleLabelChange} id="label"></input> <br /> <br />
+          <label>Number of Images</label> <br />
+          <input type="text" size="100" value={this.state.count} onChange={this.handleCountChange} id="count"></input> <br /> <br />
+          <input type="button" class="myButton_create" value="Generate" onClick={this.Generate} />
+          <input type="button" class="myButton_create" value="Save" onClick={this.Save} />
+          <input type="button" class="myButton_create" value="Cancel" onClick={this.Cancel} />
+          <br />
 
           <div class="box"> Filter By Label</div>
           <div class="box">
@@ -156,25 +154,25 @@ canShowImage(file) {
               ))}
             </select>
           </div>
-        
-          </div>
 
-          <div class="wrapper">
-          <div className="home">
-          <div class="parent">
-            {imageList.map((file) => (
-              this.canShowImage(file) &&
-              <div class="child">
-                <img src={file.src} alt="" />
-                <br />
-                <svg width="100%" height="1">
-                <rect width="100%" height="1" />
-                </svg>
-                <br />
-              </div>
-            ))}
-          </div>
         </div>
+
+        <div class="wrapper">
+          <div className="home">
+            <div class="parent">
+              {imageList.map((file) => (
+                this.canShowImage(file) &&
+                <div class="child">
+                  <img src={file.src} alt="" />
+                  <br />
+                  <svg width="100%" height="1">
+                    <rect width="100%" height="1" />
+                  </svg>
+                  <br />
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
 
       </div>
--- FrontEnd/client/src/components/DisplayImages.jsx
+++ FrontEnd/client/src/components/DisplayImages.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { withRouter } from 'react-router-dom';
 import queryString from 'query-string';
-import Select from 'react-select';
+// import Select from 'react-select';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import ReactLoading from "react-loading";
